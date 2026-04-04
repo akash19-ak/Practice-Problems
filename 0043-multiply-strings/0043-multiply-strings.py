d0310b632ad1dc9cc1cc09805a86be24e39f1fd3
AKASH NAYAK <154588453+akash19-ak@users.noreply.github.com>
2026-04-04 16:10:36 +0530

    Time: 0 ms (100%), Space: 12.4 MB (34.51%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        s=int(num1)
+        d=int(num2)
+        return str(s*d)
