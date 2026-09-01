4d76432bf68423398d5b60d187f8e4aa71f7e335
AKASH NAYAK <154588453+akash19-ak@users.noreply.github.com>
2026-09-01 19:30:31 +0530

    Time: 4 ms (55.1%), Space: 19.2 MB (61.04%) - LeetHub

---
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
@@ -0,0 +1,16 @@
+# Remove Linked List Elements
+# Purpose: Remove all nodes whose value equals val.
+
+class Solution:
+    def removeElements(self, head, val):
+        dummy = ListNode(0)
+        dummy.next = head
+        current = dummy
+
+        while current.next:
+            if current.next.val == val:
+                current.next = current.next.next
+            else:
+                current = current.next
+
+        return dummy.next
